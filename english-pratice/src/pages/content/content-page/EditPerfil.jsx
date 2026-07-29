da1a0a3fc5683e66e5d1e6960eed2037b0f08ec8
murilo <murilopaula2008@gmail.com>
2026-07-28 21:20:49 -0300

    Bug's vertical scroll fixed

--- english-pratice/src/pages/content/content-page/EditPerfil.jsx
+++ english-pratice/src/pages/content/content-page/EditPerfil.jsx
@@ -13,25 +13,30 @@ const EditPerfil = () => {
     if (newName.trim() !== "") updateUser({ name: newName });
     if (newEmail.trim() !== "") updateUser({ email: newEmail });
     if (newId.trim() !== "") updateUser({ id: newId });
+    setNewName('');
+    setNewEmail('');
+    setNewId('')
    ;
   };
   
   return (
-    <form className="grid md:grid-cols-3 w-screen h-screen">
+    <form className="grid md:grid-cols-3 w-full h-full">
       <div className="w-full col-span-1 flex justify-center bg-amber-50 border-r">
-        <div>
-          <div className="w-40 h-40 rounded-full overflow-hidden m-5 mt-10">
-            <img className="w-full h-full object-cover" src={user.avatar} alt="" />
+        <div className="md:w-72 flex md:flex-col items-center">
+          <div>
+            <div>
+              <div className="w-28 h-28 md:w-28 md:h-38 rounded-full overflow-hidden m-5 mt-10">
+                <img className="w-full h-full object-cover" src={user.avatar} alt="" />
+              </div>
+              <div className="flex justify-center">
+                <label className="w-20 h-12 md:w-24 md:h-14 bg-emerald-500 rounded flex items-center justify-center text-white text-sm cursor-pointer" htmlFor="avatarinput">Choose photo</label>
+            
+                <input className="hidden" type="file" name="" id="avatarinput" accept="image/*" onChange={(e) => updateUser( {avatar: URL.createObjectURL(e.target.files[0])})}/>
+              </div>
+            </div>
           </div>
 
-          <div className="flex justify-center">
-            <label className="w-24 h-14 bg-emerald-500 rounded flex items-center justify-center text-white text-sm cursor-pointer" htmlFor="avatarinput">Choose photo</label>
-
-          
-            <input className="hidden" type="file" name="" id="avatarinput" accept="image/*" onChange={(e) => updateUser( {avatar: URL.createObjectURL(e.target.files[0])})}/>
-          </div>
-
-          <div className="max-w-50">
+          <div className="w-full">
             <p className="break-all"><strong>Name:</strong> {user.name}</p>
             <p className="break-all"><strong>Email:</strong> {user.email}</p>
             <p className="break-all"><strong>CPF:</strong> {user.id ? user.id : "Not information"}</p>
@@ -44,15 +49,15 @@ const EditPerfil = () => {
           <h1 className="text-3xl font-bold">Accont Settings</h1>
           <div>
             <h2>Your Name</h2>
-            <input className="border w-full h-8 rounded" maxLength="25" type="text" name="name" id="" placeholder={user.name} onChange={(e) => setNewName(e.target.value)}/>
+            <input className="border w-full h-8 rounded" maxLength="25" type="text" name="name" value={newName} id="" placeholder="Enter your name" onChange={(e) => setNewName(e.target.value)}/>
           </div>
           <div>
             <h2>Email address</h2>
-            <input className="border w-full h-8 rounded" maxLength="25" type="text" name="email_address" id="" placeholder={user.email} onChange={(e) => setNewEmail(e.target.value)}/>
+            <input className="border w-full h-8 rounded" maxLength="40" type="text" name="email_address" value={newEmail} id="" placeholder="example@email.com" onChange={(e) => setNewEmail(e.target.value)}/>
           </div>
           <div>
             <h2>ID</h2>
-            <input className="border w-full h-8 rounded" maxLength="15" type="text"  inputMode="numeric" name="id" id="" placeholder={user.id} onChange={(e) => setNewId(e.target.value.replace(/\D/g, ""))}/>
+            <input className="border w-full h-8 rounded" maxLength="15" type="text"  inputMode="numeric" name="id" value={newId} id="" placeholder="Enter your ID" onChange={(e) => setNewId(e.target.value.replace(/\D/g, ""))}/>
           </div>
         </div>
 
